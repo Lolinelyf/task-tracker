8f1cb454ad1e747072dc17cd87b10e6edb2de2fd
Lolinelyf <lolinelyf@mail.ru>
2026-07-28 13:00:21 +0300

    feat: create
    Создал пустой проект, предварительно установил зависимости без настройки

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+    Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Пояс толку
+    языком океана. Пустился пояс он, себя эта то речью вершину до даже силуэт, снова ему, переулка
+    безопасную не.
+  </p>
+</template>
